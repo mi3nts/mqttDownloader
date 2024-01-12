1d31fbdc03a1ffc656d7c2c1f15f4886a3de25e2
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2024-01-12 15:47:30 -0600

    latest updates

--- firmware/mintsXU4/mintsDefinitions.py
+++ firmware/mintsXU4/mintsDefinitions.py
@@ -10,60 +10,6 @@ from mintsXU4 import mintsDownloader as mDL
 import tkinter as tk
 from tkinter import filedialog
 
-# def load_yaml_file(file_path):
-#     with open(file_path, 'r') as file:
-#         data = yaml.safe_load(file)
-#     return data
-
-# def choose_file(messageStr,fileExt):
-#     # Create the Tkinter window
-#     root = tk.Tk()
-#     root.title(messageStr)
-
-#     # Create a label with the custom message
-#     label = tk.Label(root, text=messageStr)
-#     label.pack(pady=10)
-
-#     # Hide the Tkinter window
-#     root.iconify()
-#     messagebox.showinfo("Message", messageStr)
-
-#     # Open the file dialog
-#     file_path =  filedialog.askopenfilename(title= messageStr,filetypes=[("fileType", "*."+ fileExt)])
-
-#     # Destroy the Tkinter window
-#     root.destroy()
-
-#     if file_path:
-#         return file_path
-#     else:
-#         quit()
-
-# def choose_folder(messageStr):
-#     # Create a Tkinter window
-#     root = tk.Tk()
-#     root.title(messageStr)
-
-#     # Create a label with the custom message
-#     label = tk.Label(root, text=messageStr)
-#     label.pack(pady=10)
-
-#     # Hide the Tkinter window
-#     root.iconify()
-
-#     # Open the folder dialog
-#     messagebox.showinfo("Message", messageStr)
-
-#     folder_path = filedialog.askdirectory()
-
-#     # Destroy the Tkinter window
-#     root.destroy()
-
-#     if folder_path:
-#         return folder_path
-#     else:
-#         quit()
-
 dataFolder                = mDL.choose_folder("Please choose a downloads directory")
 dataFolderReference       = dataFolder+ "/reference"
 dataFolderMQTTReference   = dataFolder+ "/referenceMqtt"  # The path of your MQTT Reference Data 
@@ -73,14 +19,14 @@ tlsCertsFile              = mDL.choose_file("Please choose TLS Certs file","crt"
 latestOn                  = False
 mqttOn                    = True
 
-credentials               = mDL.load_yaml_file(mDL.choose_file("Please choose the mints credentials file","*.yaml"))
+credentials               = mDL.load_yaml_file(mDL.choose_file("Please choose the mints credentials file","yaml"))
 
 sensorInfo                = pd.read_csv('https://raw.githubusercontent.com/mi3nts/mqttSubscribersV2/main/lists/sensorIDs.csv')
 portInfo                  = pd.read_csv('https://raw.githubusercontent.com/mi3nts/mqttSubscribersV2/main/lists/portIDs.csv')
 # nodeInfo                  = pd.read_csv('https://raw.githubusercontent.com/mi3nts/AirQualityAnalysisWorkflows/main/influxdb/nodered-docker/id_lookup.csv')
 
 
-nodeInfo                   = mDL.load_yaml_file(mDL.choose_file("Please choose the MINTS Nodes List","*.yaml"))
+nodeInfo                   = mDL.load_yaml_file(mDL.choose_file("Please choose the MINTS Nodes List","yaml"))
 # nodeInfoDC                 = load_yaml_file(choose_file("Please choose the mints DC Nodes List","*.yaml"))
 # nodeInfoLN                 = load_yaml_file(choose_file("Please choose the mints LN Nodes List","*.yaml"))
 
